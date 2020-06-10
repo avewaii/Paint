76066a9f3bbfba277c75d5a0c5035718aafb38e1
arktic <admin@arktic.ru>
2020-06-10 18:59:38 +0300

    Rename jsx to js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import Grid from '@material-ui/core/Grid';
 import Header from './components/Header';
 import Tools from './components/toolButtons';
-import Content from './components/Content';
+import Canvas from './components/Canvas';
 import DiscreteSlider from './components/ChangeWidthOfBrush';
 
 export default class App extends React.Component {
@@ -25,7 +25,7 @@ export default class App extends React.Component {
                     <Grid item xs={0} sm={3}/>
                     <Grid item container xs={12} sm={6} direction='column' spacing={3}>
                         <Grid item>
-                            <Content/>
+                            <Canvas/>
                         </Grid>
                         <Grid item>
                             <DiscreteSlider/>
