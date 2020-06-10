3ffe8c4aef111f92790fceb805f8bb560aadecb8
arktic <admin@arktic.ru>
2020-06-10 18:46:29 +0300

    Move app logic to App.jsx

--- src/index.js
+++ src/index.js
@@ -1,41 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./App";
 
-import Grid  from '@material-ui/core/Grid';
-
-import Header from './components/Header';
-import Tools from './components/toolButtons';
-import Content from './components/Content';
-import  DiscreteSlider from './components/ChangeWidthOfBrush';
-
-function App() {
-  return (
-    <Grid container direction='column' spacing='3'>
-      <Grid item>
-        <Header/>
-      </Grid>
-      <Grid item container spacing={2}>
-        <Grid item xs={0} sm={3}/>
-        <Grid item xs={12} sm={6}>
-          <Tools/>
-        </Grid>
-        <Grid item xs={0} sm={3}/>
-      </Grid>
-      <Grid item container>
-        <Grid item xs={0} sm={3}/>
-        <Grid item container xs={12} sm={6} direction='column' spacing={3}>
-          <Grid item>
-            <Content/>
-          </Grid>
-          <Grid item>
-            <DiscreteSlider/>
-          </Grid>   
-        </Grid>
-        <Grid item xs={0} sm={3}/>
-      </Grid>  
-    </Grid>
-    
-  );
-}
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App/>, document.querySelector('#root'));
