1145c26576ec135cbf076b9f8a54df1e71e626bc
avewaii <ewashadevsk@gmail.com>
2020-06-14 15:14:14 +0300

    refactor: change app structure

---
+++ src/components/Canvas.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Canvas.css"
+
+
+export default class Canvas extends React.Component {
+    render() {
+        return (
+            <canvas/>
+        )
+    }
+}
